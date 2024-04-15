66614ce2fa745c70d5adff02357c4a3d6210df50
Johan Sangalang <jnsangalang93@yahoo.com>
2024-04-15 22:04:57 +0000

    edits to layout, for responsiveness on mobile devices

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
+'use strict';
 // /* global data */
 // const $imageSubmit = document.querySelector(".image-submit");
 // const $photoInput = document.querySelector(".photo-input");
-
 // if (!$imageSubmit) throw new Error('The $imageSubmit query failed');
 // if (!$photoInput) throw new Error('The $photoInput query failed');
-
 // $photoInput.addEventListener('input' , (event:Event)=>{
 //   // $photoUrl.textContent = $photoInput.textContent;
 //   if($imageSubmit !== null){
