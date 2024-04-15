d3481aaa0300ae61a31dc6a8c8d23fc23b406d40
Johan Sangalang <jnsangalang93@yahoo.com>
2024-04-15 22:51:51 +0000

    created function to take user input and changed src attribute of preview image

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 'use strict';
-// /* global data */
-// const $imageSubmit = document.querySelector(".image-submit");
-// const $photoInput = document.querySelector(".photo-input");
-// if (!$imageSubmit) throw new Error('The $imageSubmit query failed');
-// if (!$photoInput) throw new Error('The $photoInput query failed');
-// $photoInput.addEventListener('input' , (event:Event)=>{
-//   // $photoUrl.textContent = $photoInput.textContent;
-//   if($imageSubmit !== null){
-//   if (!$imageSubmit) throw new Error('The $imageSubmit query failed');
-//       $imageSubmit.src= $photoInput.value;
-//   }
-//   }
-// )
+/* global data */
+const $imageSubmit = document.querySelector('.image-submit');
+const $photoInput = document.querySelector('.photo-input');
+if (!$imageSubmit) throw new Error('The $imageSubmit query failed');
+if (!$photoInput) throw new Error('The $photoInput query failed');
+$photoInput.addEventListener('input', (event) => {
+  // $photoUrl.textContent = $photoInput.textContent;
+  if (!$imageSubmit) throw new Error('The $imageSubmit query failed');
+  const eventTarget = $photoInput.value;
+  $imageSubmit.setAttribute('src', eventTarget);
+});
--- ts/main.ts
+++ ts/main.ts
@@ -1,15 +1,15 @@
 // /* global data */
 // const $imageSubmit = document.querySelector(".image-submit");
-// const $photoInput = document.querySelector(".photo-input");
+// const $photoInput = document.querySelector(".photo-input") as HTMLFormElement;
 
 // if (!$imageSubmit) throw new Error('The $imageSubmit query failed');
 // if (!$photoInput) throw new Error('The $photoInput query failed');
 
 // $photoInput.addEventListener('input' , (event:Event)=>{
 //   // $photoUrl.textContent = $photoInput.textContent;
-//   if($imageSubmit !== null){
 //   if (!$imageSubmit) throw new Error('The $imageSubmit query failed');
-//       $imageSubmit.src= $photoInput.value;
+//      const eventTarget= $photoInput.value;
+//       $imageSubmit.setAttribute('src' , eventTarget);
 //   }
-//   }
+
 // )
